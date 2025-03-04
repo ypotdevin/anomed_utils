5b0da79e5bb658c40aabdc220658c68011b56efb
Yannik Potdevin <yannik.potdevin@uni-luebeck.de>
2025-03-04 14:46:14 +0100

    Prepare Read the Docs integration.

---
+++ docs/conf.py
@@ -0,0 +1,35 @@
+import os
+import sys
+
+sys.path.append(".")
+
+project = "AnoMed Utilities"
+copyright = "2025, Yannik Potdevin"
+author = "Yannik Potdevin"
+
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.napoleon",  # to parse NumPy docstrings
+    "myst_parser",  # to parse .md files too
+    "autodoc2",  # this is more compatible with MyST than Sphinx' autodoc
+]
+
+templates_path = ["_templates"]
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "/")
+html_theme = "sphinx_rtd_theme"
+
+myst_enable_extensions = [
+    "fieldlist",
+]
+
+autodoc2_packages = [
+    "../src/anomed_utils",
+]
+autodoc2_docstring_parser_regexes = [
+    # this will render all docstrings as Markdown
+    (r".*", "docstrings_parser"),
+]
+autodoc2_module_all_regexes = [
+    r"anomed_utils\..*",
+]
+autodoc2_hidden_objects = ["undoc", "private", "inherited"]
